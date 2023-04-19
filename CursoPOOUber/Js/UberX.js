44a56d308d2d86b252fa9b147e6e5ac151d3de61
Rodrigo Mtz <roes2207@gmail.com>
2023-04-19 12:38:19 -0600

    Se termino la herencia en js

---
+++ CursoPOOUber/Js/UberX.js
@@ -0,0 +1,6 @@
+class UberX extends Car {
+    constructor(license, driver, brand, model) {
+        super(license, driver)
+        this.brand = brand;
+    }
+}
--- CursoPOOUber/Js/index.js
+++ CursoPOOUber/Js/index.js
@@ -1,3 +1,7 @@
-const car = new Car("AEW345", new Account("Andres Herrera", "HIW908"));
+const car = new Car("AEW345", new Account("Andres Herrera", "HIW908"))
 car.passenger = 4;
 car.printDataCar();
+
+const uberX = new UberX('JFN123', new Account('Juanito Alcachofa', 'SDF234'), 'Chevrolet', 'Spark')
+uberX.passenger = 4
+uberX.printDataCar(); 
